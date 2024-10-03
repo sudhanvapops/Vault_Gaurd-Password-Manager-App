3dc8ac6d5922104306fa7828d03266232afad4d9
Sudhanva <sudhanvapops@gmail.com>
2024-10-03 16:15:26 +0530

    Made a little progress in the ui

---
+++ components/Mainui/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Mainui = () => {
+    return (
+        <>
+            <div>
+                <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-red-400 opacity-20 blur-[100px]"></div></div>
+            </div>
+
+            <div className='flex gap-2 justify-center items-center my-10'>
+                <img src="/images/key.png" className='w-10' alt="" />
+                <h1 className='font-bold text-3xl'><span className='font-bold text-4xl text-green-700'>&lt;</span>Key<span className='text-green-700'>Guard/</span><span className='font-bold text-4xl text-green-700'>&gt;</span></h1>
+            </div>
+
+            <div>
+                jke
+            </div>
+            
+        </>
+    )
+}
+
+export default Mainui
---
+++ components/Navbar/page.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-around font-bold text-3xl bg-red-500 p-2  text-white items-center'>
+            <div className='flex items-center justify-center'>
+                <img src="/images/key.png" className='w-10 invert' alt="" />
+                <h1 className='mx-2'>
+                    {/* Here I can Use Image from Next/Image */}
+                    KeyGuard
+                </h1>
+                
+            </div>
+            <ul className='flex mx-4 gap-3 font-bold text-xl'>
+                <li className='cursor-pointer'>Home</li>
+                <li className='cursor-pointer'>About</li>
+                <li className='cursor-pointer'>Contact</li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Navbar
